83f674bfea89320b5a3726473ab26ec1b47afae9
zhangxingyu <rpm@live.com>
2021-03-12 18:05:50 +0800

    [2.2.3][开发社区登陆模块] - 开发登录退出功能 - service层开发

--- src/main/java/com/nowcoder/community/service/UserService.java
+++ src/main/java/com/nowcoder/community/service/UserService.java
@@ -4,6 +4,7 @@ import java.util.*;
 
 import com.nowcoder.community.dao.LoginTicketMapper;
 import com.nowcoder.community.dao.UserMapper;
+import com.nowcoder.community.entity.LoginTicket;
 import com.nowcoder.community.entity.User;
 import com.nowcoder.community.util.CommunityConstant;
 import com.nowcoder.community.util.CommunityUtil;
@@ -135,5 +136,60 @@ public class UserService implements CommunityConstant {
         }
     }
 
+    // 用户登录服务
+    @Autowired
+    private LoginTicketMapper loginTicketMapper;
 
+    public Map<String, Object> login(String username, String password, int expiredSecond) {
+
+        Map<String, Object> map = new HashMap<>();
+
+        // 空值处理
+
+        if (StringUtils.isBlank(username)) {
+            map.put("usernameMsg", "账号不能为空！");
+            return map;
+        }
+        if (StringUtils.isBlank(password)) {
+            map.put("passwordMsg", "密码不能为空！");
+            return map;
+        }
+
+
+        // 账号合法性验证
+
+        // 检查账号是否存在
+        User user = userMapper.selectByName(username);
+        if (user == null) {
+            map.put("usernameMsg", "该账号不存在！");
+            return map;
+        }
+
+        // 验证账号是否已经激活
+        if (user.getStatus() == 0) {
+            map.put("usernameMsg", "该账号未激活！");
+            return map;
+        }
+
+        // 验证密码
+        password = CommunityUtil.md5(password + user.getSalt());
+        if (!user.getPassword().equals(password)) {
+            map.put("pawwordMsg", "密码不正确！");
+            return map;
+        }
+
+
+        // 账号密码合法，生成登陆凭证
+
+        LoginTicket loginTicket = new LoginTicket();
+        loginTicket.setUserId(user.getId());
+        loginTicket.setTicket(CommunityUtil.generateUUID());
+        loginTicket.setStatus(0);
+        loginTicket.setExpired(new Date(System.currentTimeMillis() + expiredSecond * 1000));
+        loginTicketMapper.insertLoginTicket(loginTicket);
+        // 登陆成功
+        map.put("ticket", loginTicket.getTicket());
+
+        return map;
+    }
 }
