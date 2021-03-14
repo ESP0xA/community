5b6aa82e55989858d62249318d88cf0f3adc7f3d
zhangxingyu <rpm@live.com>
2021-03-14 16:20:49 +0800

    [2.2.7][开发社区登陆模块] - 显示登陆信息 - 测试和配置拦截器

---
+++ src/main/java/com/nowcoder/community/config/WebMvcConfig.java
@@ -0,0 +1,37 @@
+package com.nowcoder.community.config;
+
+import com.nowcoder.community.controller.Interceptor.AlphaInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * 配置拦截器和一般的配置类不同，一般的配置类为的是配置一个第三方的Bean。而这里需要实现一个接口。
+ */
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    // 配置拦截器，需要把拦截器注入进来
+    @Autowired
+    private AlphaInterceptor alphaInterceptor;
+
+    /**
+     *
+     * @param registry  Spring在调用的时候会把该对象传进来，我们利用该对象注册拦截器
+     */
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+
+        // 拦截器会拦截一切请求
+        // registry.addInterceptor(alphaInterceptor);
+
+        // 拦截器只拦截符合如下规则的请求
+        registry.addInterceptor(alphaInterceptor)
+                // /** 表示static目录下所有文件夹，这个会匹配static目录下所有的css、js和图片文件
+                .excludePathPatterns("/*/*.css", "/*/*.js", "/*/*.png", "/*/*.jpg", "/*/*.jpeg")
+                // 拦截器拦截指定路径的请求
+                .addPathPatterns("/register", "/login");
+
+    }
+}
---
+++ src/main/java/com/nowcoder/community/controller/Interceptor/AlphaInterceptor.java
@@ -0,0 +1,66 @@
+package com.nowcoder.community.controller.Interceptor;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * 拦截器
+ */
+@Component
+public class AlphaInterceptor implements HandlerInterceptor {
+
+    private static final Logger logger = LoggerFactory.getLogger(AlphaInterceptor.class);
+
+    /**
+     * 在Controller处理请求的代码之前执行的
+     * @param request
+     * @param response
+     * @param handler   拦截目标
+     * @return  请求是否被继续执行。如果返回false，则Controller不会继续往下执行。
+     * @throws Exception
+     */
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+
+
+        // 看一下handler是个什么东西
+        logger.debug("preHandle: " + handler.toString());
+        return true;
+    }
+
+
+    /**
+     * 在调用完Controller之后，模板引擎前执行
+     * @param request
+     * @param response
+     * @param handler   拦截目标
+     * @param modelAndView
+     * @throws Exception
+     */
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+
+        logger.debug("postHandle: "  + handler.toString());
+    }
+
+
+    /**
+     * 在模板引擎后最后执行
+     * @param request
+     * @param response
+     * @param handler   拦截目标
+     * @param ex
+     * @throws Exception
+     */
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+
+        logger.debug("afterCompletion: " + handler.toString());
+    }
+}
